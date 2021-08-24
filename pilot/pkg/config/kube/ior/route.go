40db2fa1d24c5e1d81642e92320033ce064d1ede
Jonh Wendell <jonh.wendell@redhat.com>
2021-08-24 07:44:47 -0400

    manageRoute

--- pilot/pkg/config/kube/ior/route.go
+++ pilot/pkg/config/kube/ior/route.go
@@ -18,6 +18,7 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -48,6 +49,7 @@ const (
 	gatewayNameLabel            = maistraPrefix + "gateway-name"
 	gatewayNamespaceLabel       = maistraPrefix + "gateway-namespace"
 	gatewayResourceVersionLabel = maistraPrefix + "gateway-resourceVersion"
+	ShouldManageRouteAnnotation = maistraPrefix + "manageRoute"
 )
 
 type syncRoutes struct {
@@ -293,6 +295,21 @@ func (r *route) handleAdd(cfg config.Config) error {
 	return result.ErrorOrNil()
 }
 
+func isManagedByIOR(cfg config.Config) (bool, error) {
+	manageRouteValue, ok := cfg.Annotations[ShouldManageRouteAnnotation]
+	if !ok {
+		// Manage routes by default, when annotation is not found.
+		return true, nil
+	}
+
+	manageRoute, err := strconv.ParseBool(manageRouteValue)
+	if err != nil {
+		return false, fmt.Errorf("could not parse annotation %q: %s", ShouldManageRouteAnnotation, err)
+	}
+
+	return manageRoute, nil
+}
+
 func (r *route) handleDel(cfg config.Config) error {
 	var result *multierror.Error
 
@@ -318,6 +335,15 @@ func (r *route) handleDel(cfg config.Config) error {
 func (r *route) handleEvent(event model.Event, cfg config.Config) error {
 	// Block until initial sync has finished
 	<-r.initialSyncRun
+
+	manageRoute, err := isManagedByIOR(cfg)
+	if err != nil {
+		return err
+	}
+	if !manageRoute {
+		IORLog.Infof("Ignoring Gateway %s/%s as it is not managed by Istiod", cfg.Namespace, cfg.Name)
+		return nil
+	}
 
 	switch event {
 	case model.EventAdd:
@@ -412,7 +438,7 @@ func (r *route) createRoute(metadata config.Meta, gateway *networking.Gateway, o
 		originalHostAnnotation: originalHost,
 	}
 	for keyName, keyValue := range metadata.Annotations {
-		if !strings.HasPrefix(keyName, "kubectl.kubernetes.io") {
+		if !strings.HasPrefix(keyName, "kubectl.kubernetes.io") && keyName != ShouldManageRouteAnnotation {
 			annotations[keyName] = keyValue
 		}
 	}
--- pkg/servicemesh/federation/discovery/discovery.go
+++ pkg/servicemesh/federation/discovery/discovery.go
@@ -27,6 +27,7 @@ import (
 	rawnetworking "istio.io/api/networking/v1alpha3"
 	rawsecurity "istio.io/api/security/v1beta1"
 	rawtype "istio.io/api/type/v1beta1"
+	"istio.io/istio/pilot/pkg/config/kube/ior"
 	"istio.io/istio/pkg/config"
 	"istio.io/istio/pkg/config/schema/collection"
 	"istio.io/istio/pkg/config/schema/collections"
@@ -364,6 +365,7 @@ func (c *Controller) discoveryIngressGateway(instance *v1.ServiceMeshPeer) *conf
 			GroupVersionKind: collections.IstioNetworkingV1Alpha3Gateways.Resource().GroupVersionKind(),
 			Name:             name,
 			Namespace:        instance.Namespace,
+			Annotations:      map[string]string{ior.ShouldManageRouteAnnotation: "false"},
 		},
 		Spec: &rawnetworking.Gateway{
 			Selector: federationIngressLabels(instance),
@@ -399,6 +401,7 @@ func (c *Controller) discoveryEgressGateway(instance *v1.ServiceMeshPeer) *confi
 			GroupVersionKind: collections.IstioNetworkingV1Alpha3Gateways.Resource().GroupVersionKind(),
 			Name:             name,
 			Namespace:        instance.Namespace,
+			Annotations:      map[string]string{ior.ShouldManageRouteAnnotation: "false"},
 		},
 		Spec: &rawnetworking.Gateway{
 			Selector: federationEgressLabels(instance),
